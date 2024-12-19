07a74ff61c6a4c8f0c0933513757e642c78d1309
Oswaldo <rodriosw13@gmail.com>
2024-12-19 19:45:45 -0400

    terminando la lógica

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 /** Setting a gameBoard module (iiefs) */
+/*** Este modulo se encarga solamente de lo que es el renderizado del juego como tal, o sea, que el juego se muestre ***/
 const gameBoard = (() => {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
 
   const render = () => {
-  let boardHTML = '';
+  let boardHTML = ''
   gameboard.forEach((square, index) => {
     boardHTML += `<div class="square" id="square-${index}">${square}</div>`
-  });
-};
+    });
+    
+    document.querySelector('#gameboard').innerHTML = boardHTML;
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+      square.addEventListener('click', Game.handleClick)
+    });
+  };
 
-  document.querySelector('#gameboard').innerHTML = boardHTML;
+  const update = (index, value) => {
+    gameboard[index] = value;
+    render();
+  };
 
-  return {
-    render
-  }
+  const getGameBoard = () => gameboard;
+
+  return { render, update, getGameBoard };
+
 })();
 
+/** Setting a factory function for the creation of the players **/
 const createPlayer = (name, mark) => {
   return {
     name,
@@ -23,10 +35,11 @@ const createPlayer = (name, mark) => {
   };
 };
 
+/** Setting a game module (iiEFs) **/
 const Game = (() => {
   let players = [];
-  let currentPlayerIndex = 0;
-  let gameOver = false;
+  let currentPlayerIndex;
+  let gameOver;
 
   const start = () => {
     players = [
@@ -34,11 +47,38 @@ const Game = (() => {
       createPlayer(document.querySelector('#player2').value, 'O')
     ];
     currentPlayerIndex = 0; 
+    gameOver = false;
+    gameBoard.render();
   };
+
+  const handleClick = (event) => {
+    let index = parseInt(event.target.id.split('-')[1]);
+    if(gameBoard.getGameBoard()[index] !== '') {
+      return;
+    }
+    gameBoard.update(index, players[currentPlayerIndex].mark);
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+  };
+
+  const restart = () => {
+    for(let i = 0; i < 9; i++) {
+      gameBoard.update(i, "");
+    };
+    gameBoard.render();
+  };
+
+  return { start, handleClick, restart }
 })();
+
+const restartBtn = document.querySelector('#restart-button');
+restartBtn.addEventListener('click', () => {
+  Game.restart();
+})
 
 
 const startButton = document.querySelector('#start-button');
 startButton.addEventListener('click', () => {
     Game.start();
+    console.log('Hola mundo')
+    
 });
