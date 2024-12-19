9f3d599bdc00951ea0c84be3c9ccf59c974c13da
Oswaldo <rodriosw13@gmail.com>
2024-12-18 22:14:22 -0400

    blabla

---
+++ script.js
@@ -0,0 +1,44 @@
+/** Setting a gameBoard module (iiefs) */
+const gameBoard = (() => {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+  const render = () => {
+  let boardHTML = '';
+  gameboard.forEach((square, index) => {
+    boardHTML += `<div class="square" id="square-${index}">${square}</div>`
+  });
+};
+
+  document.querySelector('#gameboard').innerHTML = boardHTML;
+
+  return {
+    render
+  }
+})();
+
+const createPlayer = (name, mark) => {
+  return {
+    name,
+    mark
+  };
+};
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex = 0;
+  let gameOver = false;
+
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector('#player1').value, 'X'),
+      createPlayer(document.querySelector('#player2').value, 'O')
+    ];
+    currentPlayerIndex = 0; 
+  };
+})();
+
+
+const startButton = document.querySelector('#start-button');
+startButton.addEventListener('click', () => {
+    Game.start();
+});
